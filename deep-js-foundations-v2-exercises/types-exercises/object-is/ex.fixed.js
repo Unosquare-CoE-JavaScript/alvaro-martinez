cec2685e62fc322f3b600c25d173c71c7cbb7b19
Alvaro Martinez <alvaro.martinez@REM-MC-210906A.local>
2021-09-20 14:20:08 -0500

    resolve part of problem, TODO check -0 types

--- deep-js-foundations-v2-exercises/types-exercises/object-is/ex.fixed.js
+++ deep-js-foundations-v2-exercises/types-exercises/object-is/ex.fixed.js
@@ -1,38 +1,43 @@
-// TODO: define polyfill for `Object.is(..)`
+if (!Object.is /*|| true*/) {
+	Object.is = function ObjectIs(x,y) {
+		var xNegZero = isItNegZero(x);
+		var yNegZero = isItNegZero(y);
 
-if(!Object.is || true){
-				Object.is = function ObjectIs(l, r){
-								console.log(typeof l, typeof r, {l,r})
-								if(-Infinity/l ===Infinity || -Infinity/r === Infinity ){
-												return false
-								}
-								else if(l === l  || r ===r){
-												return true  
-								}
-								else if(l===r){
-												return true
-								}
-								return false 
-				}	
+		if (xNegZero || yNegZero) {
+			return xNegZero && yNegZero;
+		}
+		else if (isItNaN(x) && isItNaN(y)) {
+			return true;
+		}
+		else if (x === y) {
+			return true;
+		}
+
+		return false;
+
+		// **********
+
+		function isItNegZero(x) {
+			return x === 0 && (1 / x) === -Infinity;
+		}
+
+		function isItNaN(x) {
+			return x !== x;
+		}
+	};
 }
 
+
 // tests:
-
-
 console.log(Object.is(42,42) === true);
 console.log(Object.is("foo","foo") === true);
 console.log(Object.is(false,false) === true);
-console.log("nullvs null");
 console.log(Object.is(null,null) === true);
-
-console.log("nullvs null");
 console.log(Object.is(undefined,undefined) === true);
 console.log(Object.is(NaN,NaN) === true);
 console.log(Object.is(-0,-0) === true);
-
-console.log("nullvs null");
 console.log(Object.is(0,0) === true);
-console.log(0,-0);
+
 console.log(Object.is(-0,0) === false);
 console.log(Object.is(0,-0) === false);
 console.log(Object.is(0,NaN) === false);
@@ -43,4 +48,3 @@ console.log(Object.is("foo","bar") === false);
 console.log(Object.is(false,true) === false);
 console.log(Object.is(null,undefined) === false);
 console.log(Object.is(undefined,null) === false);
-
