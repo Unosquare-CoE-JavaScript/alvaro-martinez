28da971c667753ee06a7715e09990170032a870f
Alvaro Martinez <alvaro.martinez@unosquare.com>
2021-09-21 12:13:27 -0500

    scope: functions-expressions: print records

--- deep-js-foundations-v2-exercises/scope-exercises/function-expressions/ex.js
+++ deep-js-foundations-v2-exercises/scope-exercises/function-expressions/ex.js
@@ -1,80 +1,49 @@
 function printRecords(recordIds) {
-				const foundedStudents = []
-			if(Array.isArray(recordIds)){	
-				recordIds.forEach(function eachStudent(id) {
-								let foundedStudent = studentRecords.find(function getId(student){
-												return student.id == id
-								} )
-
-								foundedStudents.push(foundedStudent)
-				})		
-			}
-
-				let sortedStudents = sortStudents(foundedStudents) 
-				return sortedStudents.forEach(function printStudentWithFormat(student){
-								let {id,name,paid}  = student
-								console.log(`${name} (${id}): ${paid ? 'Paid' : 'Not Paid'}`);
-				})
+  if (Array.isArray(recordIds)) {
+    let filteredStudents = recordIds.map(function getStudentById(id) {
+      return studentRecords.find(function findStudenById(student) {
+        return student.id == id ? student : undefined;
+      });
+    });
+    return filteredStudents.sort(sortStudents).forEach(printStudentWithFormat);
+  }
 }
 
-function paidStudentsToEnroll() {
-
-				let paidStudents = studentRecords.filter(function(student){
-								return currentEnrollment.includes(student.id) ? student : student.paid
-				})
-				const sortedStudents = sortStudents(paidStudents)
-				 sortedStudents.forEach(function printStudentWithFormat(student){
-								let {id,name,paid}  = student
-								console.log(`${name} (${id}): ${paid ? 'Paid' : 'Not Paid'}`);
-				})
-				let newIds= paidStudents.map(function getId(student){
-return student.id
-				})
-				console.log(newIds);
-				return newIds
-}
+function paidStudentsToEnroll() {}
 
 function remindUnpaid(recordIds) {
-				let filteredStudents;
-				console.log(recordIds);
-				if(Array.isArray(recordIds)){
-								 filteredStudents = studentRecords.filter(function getStudent(student){
-												console.log(student);
-												return recordIds.includes(student.id)  && student
-								})
-
-				}
-
-				printRecords(filteredStudents)
+  // TODO
 }
 
-
-function sortStudents (students) {
-				return students.sort(function compareStudent(studentA,studentB){
-								return studentA.name > studentB.name ? 1: -1
-				})
+function sortStudents(studentA, studentB) {
+  return studentA.name > studentB.name ? 1 : -1;
+}
+function printStudentWithFormat(student) {
+  console.log(
+    `${student.name} (${student.id}): ${student.paid ? "Paid" : "Not Paid"}`
+  );
 }
 // ********************************
 
-var currentEnrollment = [ 410, 105, 664, 375 ];
+var currentEnrollment = [410, 105, 664, 375];
 
 var studentRecords = [
-	{ id: 313, name: "Frank", paid: true, },
-	{ id: 410, name: "Suzy", paid: true, },
-	{ id: 709, name: "Brian", paid: false, },
-	{ id: 105, name: "Henry", paid: false, },
-	{ id: 502, name: "Mary", paid: true, },
-	{ id: 664, name: "Bob", paid: false, },
-	{ id: 250, name: "Peter", paid: true, },
-	{ id: 375, name: "Sarah", paid: true, },
-	{ id: 867, name: "Greg", paid: false, },
+  { id: 313, name: "Frank", paid: true },
+  { id: 410, name: "Suzy", paid: true },
+  { id: 709, name: "Brian", paid: false },
+  { id: 105, name: "Henry", paid: false },
+  { id: 502, name: "Mary", paid: true },
+  { id: 664, name: "Bob", paid: false },
+  { id: 250, name: "Peter", paid: true },
+  { id: 375, name: "Sarah", paid: true },
+  { id: 867, name: "Greg", paid: false },
 ];
 
 printRecords(currentEnrollment);
 console.log("----");
 currentEnrollment = paidStudentsToEnroll();
 printRecords(currentEnrollment);
-console.log("----",);
+console.log("----");
 remindUnpaid(currentEnrollment);
 
 /*
@@ -94,3 +63,78 @@ remindUnpaid(currentEnrollment);
 	Bob (664): Not Paid
 	Henry (105): Not Paid
 */
+
+// function printRecords(recordIds) {
+//   const foundedStudents = recordIds.map(function eachStudent(id) {
+//     return studentRecords.find(function getId(student) {
+//       return student.id == id;
+//     });
+//   });
+//   let sortedStudents = sortStudents(foundedStudents);
+//   return sortedStudents.forEach(printStudentWithFormat);
+// }
+
+// function paidStudentsToEnroll() {
+//   let paidStudents = studentRecords.filter(function (student) {
+//     return currentEnrollment.includes(student.id) ? student : student.paid;
+//   });
+//   const sortedStudents = sortStudents(paidStudents);
+//   sortedStudents.forEach(printStudentWithFormat);
+
+//   let newIds = paidStudents.map(function getId(student) {
+//     return student.id;
+//   });
+//   return newIds;
+// }
+
+// function remindUnpaid(recordIds) {
+//   let filteredStudents;
+//   if (Array.isArray(recordIds)) {
+//     filteredStudents = studentRecords.filter(function getStudent(student) {
+//       return recordIds.includes(student.id) && student;
+//     });
+//   }
+
+//   printRecords(filteredStudents);
+// }
+
+// // ********************************
+
+// var currentEnrollment = [410, 105, 664, 375];
+
+// var studentRecords = [
+//   { id: 313, name: "Frank", paid: true },
+//   { id: 410, name: "Suzy", paid: true },
+//   { id: 709, name: "Brian", paid: false },
+//   { id: 105, name: "Henry", paid: false },
+//   { id: 502, name: "Mary", paid: true },
+//   { id: 664, name: "Bob", paid: false },
+//   { id: 250, name: "Peter", paid: true },
+//   { id: 375, name: "Sarah", paid: true },
+//   { id: 867, name: "Greg", paid: false },
+// ];
+
+// printRecords(currentEnrollment);
+// console.log("----");
+// currentEnrollment = paidStudentsToEnroll();
+// printRecords(currentEnrollment);
+// console.log("----");
+// remindUnpaid(currentEnrollment);
+
+// /*
+// 	Bob (664): Not Paid
+// 	Henry (105): Not Paid
+// 	Sarah (375): Paid
+// 	Suzy (410): Paid
+// 	----
+// 	Bob (664): Not Paid
+// 	Frank (313): Paid
+// 	Henry (105): Not Paid
+// 	Mary (502): Paid
+// 	Peter (250): Paid
+// 	Sarah (375): Paid
+// 	Suzy (410): Paid
+// 	----
+// 	Bob (664): Not Paid
+// 	Henry (105): Not Paid
+// */
